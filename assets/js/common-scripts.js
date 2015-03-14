f557a6db01bb8e3a12c1f1baa4711c069f11cbba
Andrea Del Rio <delrio.andre@gmail.com>
2015-03-13 20:23:26 -0500

    refactoring menu?

--- assets/js/common-scripts.js
+++ assets/js/common-scripts.js
@@ -15,20 +15,6 @@ $(function() {
 
 var Script = function () {
 
-
-//    sidebar dropdown menu auto scrolling
-
-    jQuery('#sidebar .sub-menu > a').click(function () {
-        var o = ($(this).offset());
-        diff = 250 - o.top;
-        if(diff>0)
-            $("#sidebar").scrollTo("-="+Math.abs(diff),500);
-        else
-            $("#sidebar").scrollTo("+="+Math.abs(diff),500);
-    });
-
-
-
 //    sidebar toggle
 
     $(function() {
@@ -48,26 +34,30 @@ var Script = function () {
         $(window).on('resize', responsiveView);
     });
 
+    // $('.fa-bars').click(function () {
+    //     if ($('#sidebar > ul').is(":visible") === true) {
+    //         $('#main-content').css({
+    //             'margin-left': '0px'
+    //         });
+    //         $('#sidebar').css({
+    //             'margin-left': '-210px'
+    //         });
+    //         $('#sidebar > ul').hide();
+    //         $("#container").addClass("sidebar-closed");
+    //     } else {
+    //         $('#main-content').css({
+    //             'margin-left': '210px'
+    //         });
+    //         $('#sidebar > ul').show();
+    //         $('#sidebar').css({
+    //             'margin-left': '0'
+    //         });
+    //         $("#container").removeClass("sidebar-closed");
+    //     }
+    // });
     $('.fa-bars').click(function () {
-        if ($('#sidebar > ul').is(":visible") === true) {
-            $('#main-content').css({
-                'margin-left': '0px'
-            });
-            $('#sidebar').css({
-                'margin-left': '-210px'
-            });
-            $('#sidebar > ul').hide();
-            $("#container").addClass("sidebar-closed");
-        } else {
-            $('#main-content').css({
-                'margin-left': '210px'
-            });
-            $('#sidebar > ul').show();
-            $('#sidebar').css({
-                'margin-left': '0'
-            });
-            $("#container").removeClass("sidebar-closed");
-        }
+        $('#aside').toggleClass("menu-toggle");
+        $('#main-content').toggleClass("content-toggle");
     });
 
 // custom scrollbar
